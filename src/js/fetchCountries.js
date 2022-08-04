8c71c7322d8d7f0fb749454a41ccd9d04b27c3a2
VolodymyrBashtanyuk <vladimirbashtanuk@gmail.com>
2022-08-04 15:11:43 +0300

    task-01 complete

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,26 +1,13 @@
-const URL = 'https://restcountries.com/v2'
+const URL = 'https://restcountries.com/v2';
 
-export default class ApiCountries {
-    constructor() {
-        // this.name = '';
-    }
-     
-    fetchCountries(name) {
-        
+   export default function fetchCountries(name) {
+
         return fetch(`${URL}/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => response.json())
-        .then(dataCountries => dataCountries);
+        .then(dataCountries =>  dataCountries)
+        .catch(error => error)
+      
     };
     
-
-};
-    
-
-
-// name.official - полное имя страны name
-// capital - столица capital:
-// population - население population
-// flags.svg - ссылка на изображение флага flags:
-// languages - массив языков languages: {aym: "Aymara", que: "Quechua", spa: "Spanish"}
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,67 @@
 import '../css/styles.css';
 import Debounce from 'lodash.debounce';
-import ApiCountries from './fetchCountries'
+import Notiflix from 'notiflix';
+import apiCountries from './fetchCountries';
+
 
 const DEBOUNCE_DELAY = 300;
-const apiCountries = new ApiCountries();
+
 
 const searchContry = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list')
+const coutryInfo = document.querySelector('.country-info');
+
 searchContry.addEventListener('input', Debounce(onSearchContry, DEBOUNCE_DELAY))
 
 function onSearchContry(e) {
     const countries = e.target.value;
-   apiCountries.fetchCountries(countries.trim());
+    if (countries === '') { 
+        coutryInfo.innerHTML = '';  //очищає HTML якщо дуже швидко видаляем input
+        countryList.innerHTML = '';
+        return;
+    }
+    apiCountries(countries.trim())
+        .then(data => insertContent(data))
+        .catch(error => notFound(error));
+    
 };
-console.log(ApiCountries.dataCountries)
-// name.official - полное имя страны
-// capital - столица
-// population - население
-// flags.svg - ссылка на изображение флага
-// languages - массив языков
-// function fetchCountries(name) {
-//     const url = 'https://restcountries.com/v3.1'
-// return fetch(`${url}/name/${name}`).then(response => response.json()).then(result => console.log()).catch(error => error);
-// }
+
+const insertContent = (countries) => {
+    if (countries.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        countryList.innerHTML = '';
+        return;
+    } else if (countries.length === 1) {
+        const resultInfo = countryInfoMarkup(countries)
+        coutryInfo.insertAdjacentHTML('beforeend', resultInfo);
+    } else {
+        coutryInfo.innerHTML = '';
+    }
+     const result = listCountry(countries);
+     countryList.innerHTML = result;  
+}
+
+const listCountry = (list) => list.reduce((acc, item) => acc + countryMarkup(item), '');
+
+
+const countryMarkup = (({ name, flags }) => {
+    return `<li class="country-item">
+    <img src='${flags.svg}' alt='flag ${name}' width='60' height='40'>${name}</li>`
+});
+
+const countryInfoMarkup = (country) => {
+    for (let { capital, population, languages } of country) {
+        const language = languages.map(list => list.name).join(' ');
+        const info = `<p class="country-info__text"><span>Capital:</span>${capital}</p>
+        <p class="country-info__text"><span>Population:</span>${population}</p>
+        <p class="country-info__text"><span>Languages:</span>${language}</p>`;
+        return info;
+    }
+};
+
+const notFound = () => {
+    Notiflix.Notify.failure('Oops, there is no country with that name')
+    countryList.innerHTML = '';  
+    coutryInfo.innerHTML = ''
+};
+
